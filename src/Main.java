35475a33e55250badf492307d29c265be344104e
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 14:17:05 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Display welcome message
+        System.out.println("Welcome to Line Comparison Computation");
+        System.out.println("Program on Master Branch");
+    }
+}
